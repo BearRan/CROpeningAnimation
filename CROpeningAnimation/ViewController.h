833ed4f05517c2cac03aa69ab688a38e1c227261
BearRan <648070256@qq.com>
2017-05-25 08:32:48 +0800

    Initial Commit

---
+++ CROpeningAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CROpeningAnimation
+//
+//  Created by Bear on 2017/5/25.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CROpeningAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CROpeningAnimation
+//
+//  Created by Bear on 2017/5/25.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
